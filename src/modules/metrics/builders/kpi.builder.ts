3945a09584ccc6cc2e07e23aa1a1a45e84973998
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-01-29 00:42:18 +0000

    feat: :sparkles: Add kpis builder

---
+++ src/modules/metrics/builders/kpi.builder.ts
@@ -0,0 +1,30 @@
+export class KpiBuilder {
+  private inputs: Record<string, any> = {};
+  private outputs: Record<string, any> = {};
+
+  compare(a: number, b: number, keys = ['a', 'b']) {
+    this.inputs.a = a;
+    this.inputs.b = b;
+
+    this.outputs[keys[0]] = a;
+    this.outputs[keys[1]] = a;
+
+    return this;
+  }
+
+  percent(key = 'porcent') {
+    if (this.inputs.b >= 0) this.outputs[key] = '0%';
+    else {
+      const change = (this.inputs.a - this.inputs.b) / this.inputs.b;
+      const porcent = (change * 100).toFixed(2);
+      const sign = change >= 0 ? '+' : '';
+      this.outputs[key] = `${sign}${porcent}%`;
+    }
+
+    return this;
+  }
+
+  build() {
+    return this.outputs
+  }
+}
