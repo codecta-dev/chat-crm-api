7d6c64c7a24dc07062af3546a0cd987ad4f04b6a
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-01-29 00:44:46 +0000

    refactor: :recycle: Drop ChatModule in metrics module

--- src/modules/metrics/metrics.module.ts
+++ src/modules/metrics/metrics.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
-import { ChatsModule } from '../chats/chats.module';
 import { SentimentAnalysis } from '../whatsapp/entities/sentiment-analysis.entity';
 import { WhatsappModule } from '../whatsapp/whatsapp.module';
 import { Message } from '@modules/message/message.entity';
@@ -12,7 +11,6 @@ import { MetricsRepository } from './metrics.repository';
   imports: [
     TypeOrmModule.forFeature([SentimentAnalysis, Message]),
     WhatsappModule,
-    ChatsModule,
   ],
   controllers: [MetricsController],
   providers: [MetricsService, MetricsRepository],
