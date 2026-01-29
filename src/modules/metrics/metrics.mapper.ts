a409bd3598b687efebb5c5cbc335a17c34ef2be2
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-01-29 00:42:41 +0000

    feat: :sparkles: Add metrics mapper

---
+++ src/modules/metrics/metrics.mapper.ts
@@ -0,0 +1,24 @@
+import { AgentQuery, ClientQuery } from "./metrics.interface";
+
+export class MetricMapper {
+
+  static toAgentMetrics(queries: AgentQuery[]) {
+    return queries.map((r) => ({
+      agentId: r.agentId,
+      agentName: `${r.firstNames} ${r.lastNames}`.trim(),
+      total: Number(r.total),
+      avg: Number(r.avg) || 0,
+      score: Number(r.score) || 0
+    }));
+  }
+
+  static toBestClients(queries: ClientQuery[]) {
+    return queries.map((q) => ({
+      clientId: q.contactId,
+      clientName: q.username || 'Sin nombre',
+      totalMessages: Number(q.totalMessages),
+      avgSentiment: Number(q.avgPos) || 0,
+      score: Number(q.score) || 0
+    }));
+  }
+}
