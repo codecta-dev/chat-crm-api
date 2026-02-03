6b1a323ea960d62b1b5ddc3e845d8c07ec1130e5
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-02-03 16:38:24 +0000

    feat: :sparkles: Add metrics constants
    sentiment label (wip), compare period configs, date format sql (wip)

---
+++ src/modules/metrics/constants/metrics.constants.ts
@@ -0,0 +1,38 @@
+import { PeriodTime } from "src/lib/period";
+import { CompareMetric, Table } from "../metrics.types";
+import { SentimentLabel } from "@modules/analysis/sentiment/sentiment.enum";
+
+export const SENTIMENT_LABELS_MAP = {
+  positive: SentimentLabel.POSITIVE,
+  neutral: SentimentLabel.NEUTRAL,
+  negative: SentimentLabel.NEGATIVE,
+} as const;
+
+export const COMPARE_PERIOD_CONFIG: Record<CompareMetric, { target: Table, column: string }> = {
+  chat: {
+    column: 'id',
+    target: 'chats'
+  },
+  message: {
+    column: 'id',
+    target: 'messages'
+  },
+  transfer: {
+    column: 'id',
+    target: 'transfers'
+  },
+  agent: {
+    target: 'messages',
+    column: 'agent_id'
+  }
+} as const;
+
+// Date format strings for SQL DATE_FORMAT function based on period time
+export const DATE_FORMAT_SQL: Record<PeriodTime, string> = {
+  // date: 'date subdate' -- format
+  hour: '%H %i',
+  day: '%W %H',
+  week: "%u %W",
+  month: "%b %d",
+  year: "%Y %M",
+} as const;
