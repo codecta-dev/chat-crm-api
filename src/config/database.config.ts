31f4d90cb33625d9dd7d074138e98c9819a1abba
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-02-06 17:33:54 +0000

    feat: :wrench: Add test database config with mysql and sqlite (better-sqlit3)

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -14,3 +14,22 @@ export const databaseConfig: TypeOrmModuleOptions = {
   namingStrategy: new SnakeNamingStrategy(),
   // logging: ['query'],
 };
+
+export const testDatabaseConfig: TypeOrmModuleOptions = {
+  ...databaseConfig,
+  database: process.env.DB_DATABASE + '_test',
+  synchronize: true,
+  dropSchema: true,
+  logger: 'formatted-console',
+}
+
+export const testDatabaseSQLiteConfig: TypeOrmModuleOptions = {
+  type: 'better-sqlite3',
+  database: ':memory:',
+  synchronize: true,
+  dropSchema: true,
+  entities: [__dirname + '/../modules/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*.ts'],
+  namingStrategy: new SnakeNamingStrategy(),
+  logger: 'formatted-console'
+}
