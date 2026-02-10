9eb8fedce2ccf5bdcdadfb87dea2d811c3da803f
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-02-10 00:34:31 +0000

    feat: :sparkles: Add new table in database (chat-assigments)

---
+++ src/modules/chats/entities/chat-assignments.entity.ts
@@ -0,0 +1,34 @@
+import { Chat, User } from "@entities";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export enum ReasonAssignment {
+  TRANSFER = 'transfer',
+  ESCALATION = 'escalation',
+  MANUAL = 'manual',
+  AUTO = 'auto',
+}
+
+@Entity()
+export class ChatAssignments {
+  @PrimaryGeneratedColumn('uuid', { name: 'chat_assignment_id' })
+  id: string;
+
+  @ManyToOne(() => Chat)
+  chat: Chat;
+
+  @ManyToOne(() => User)
+  agent: User;
+
+  @Column({ type: 'datetime' })
+  assignedAt: Date;
+
+  @Column({ type: 'datetime' })
+  unassignedAt: Date;
+
+  @Column({
+    type: 'simple-enum',
+    enum: ReasonAssignment,
+    default: ReasonAssignment.AUTO
+  })
+  reason: ReasonAssignment;
+}
