66fb5b493d19a23f02198c4a4e314b2cd33d858c
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-02-17 19:03:49 +0000

    fix: :bug: Fix imports in whatsapp module for controllers

--- src/integrations/whatsapp/controllers/index.ts
+++ src/integrations/whatsapp/controllers/index.ts
@@ -1,9 +1,7 @@
 import { WhatsAppConfigController } from "./whatsapp-config.controller";
 import { WhatsappWebhookController } from "./whatsapp-webhook.controller";
-import { WhatsappController } from "./whatsapp.controller";
 
 export {
   WhatsAppConfigController,
   WhatsappWebhookController,
-  WhatsappController,
 };
