6f58530af06e8150ccaa49e0095f1a6a779c6c30
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-03-02 23:51:50 +0000

    refactor: :wrench: Config timezone for logger

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,5 +1,7 @@
 import { Params } from "nestjs-pino";
 
+process.env.TZ = process.env.APP_TZ || 'America/Lima'; // timezone
+
 export const loggerConfig: Params = {
   pinoHttp: {
     level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
@@ -8,16 +10,15 @@ export const loggerConfig: Params = {
       options: {
         singleLine: true,
         colorize: true,
-        translateTime: 'yyyy-mm-dd HH:MM:ss',
+        translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
         ignore: 'pid,hostname,context,http,statusCode',
         levelFirst: true,
-        messageFormat: '{if context}{context} | {end}{if http}{http} {end}{msg}{end}{if statusCode}{statusCode}{end}',
+        messageFormat: '{if context}{context} | {end}{if http}{http} {end}{msg}{end}',
         customColors: 'error:red,info:cyan,debug:blue,warn:yellow',
       },
     } : undefined,
     customProps: (req, res) => ({
-      http: `[${req.url}, ${req.method}]`,
-      statusCode: ` - ${res.statusCode}`,
+      http: `[${req.url}, ${req.method}, ${res.statusCode}]`,
     }),
   },
 };
